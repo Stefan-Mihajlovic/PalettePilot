4e0b346f96eaceb20eff9b3d985faf620a39177e
Stefan Mihajlovic <stef.mihajlovicc@gmail.com>
2025-04-24 17:56:51 +0200

    Version 1.0.2
    - Made icons into SVG elements and deleted the images folder
    - Made the area for opening the color popup bigger, so its easier to press
    - The color popup now has text to make it clear you can copy the color codes (HEX, RGB) and a new opening animation

--- index.js
+++ index.js
@@ -58,6 +58,7 @@ const createColorPopup = (color) => {
                     </div>
                 </div>
             </div>
+            <span class="topText"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3 16V4C3 2.89543 3.89543 2 5 2H15M9 22H18C19.1046 22 20 21.1046 20 20V8C20 6.89543 19.1046 6 18 6H9C7.89543 6 7 6.89543 7 8V20C7 21.1046 7.89543 22 9 22Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>Click the values to copy them</span>
         </div>
     `;
 
@@ -94,9 +95,8 @@ const showColors = () => {
 
     const colorElements = document.querySelectorAll('.color');
     colorElements.forEach((li) => {
-        const colorHex = li.querySelector('.value.hex');
-        colorHex.addEventListener('click', (e) => {
-            const color = e.currentTarget.dataset.color;
+        li.addEventListener('click', (e) => {
+            const color = e.currentTarget.querySelector(".value.hex").dataset.color;
             if(currentPopup){
                 document.body.removeChild(currentPopup);
             }
